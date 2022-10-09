b2409f0b23f1b6bae0f3e2a8d17ec5b2a8da9589
Jooyul Yoon <juyeolyoon@gmail.com>
2022-10-09 13:29:53 +0000

    221009 - routes(login, userinfo) added

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Chatroom from "./pages/Chatroom";
+import Navbar from "./components/Navbar";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
+import UserInfo from "./pages/UserInfo";
+import Main from "./pages/Main";
 
 function Router() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/signup" element={<Join />} />
-        <Route path="/chatroom" element={<Chatroom />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/userinfo" element={<UserInfo />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
