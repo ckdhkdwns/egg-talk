cba11f2c38d9090221a6b04e88bd783f69b72123
Jooyul Yoon <juyeolyoon@gmail.com>
2022-10-09 13:30:52 +0000

    221009 - StrictMode removed because of twice of rendering

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -9,11 +9,11 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>
+  // </React.StrictMode>
 );
