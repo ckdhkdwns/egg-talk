9818d02771bf6e2709751bacc32273c5af0f4c09
Jooyul Yoon <juyeolyoon@gmail.com>
2022-10-09 13:27:43 +0000

    221009 - api/atoms changed

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,3 +1,66 @@
-export function a() {
-  return null;
-}
+export const API_URL = "https://egg-talk-server.run.goorm.io";
+
+export type LoginFormData = {
+  userId: string;
+  password: string;
+};
+
+export type userData = {
+  authorityDtoSet: [];
+  userId: string;
+  username: string;
+  email: string;
+  gender: number;
+} | null;
+
+export type TypeRoom = {
+  createdDate?: string;
+  creatorId?: string;
+  modifiedDate?: string;
+  roomId?: number;
+  roomName?: string;
+} | null;
+
+export type TypeMessage = {
+  createdDate: string;
+  id: number;
+  messageType: number;
+  roomId: number;
+  userId: string;
+  username: string;
+  message: string;
+};
+
+export const roomsTest = [
+  {
+    createdDate: "2022-10-04T13:38:50",
+    roomId: 10,
+    creatorId: "test32",
+    roomName: "testterdf",
+  },
+  {
+    createdDate: "2022-10-04T13:53:51",
+    roomId: 11,
+    creatorId: "ckdhkdwns",
+    roomName: "My room!",
+  },
+];
+
+export const messagesTest = [
+  {
+    createdDate: "2022-10-04T10:31:19",
+    id: 4,
+    messageType: 0,
+    roomId: 6,
+    sender: "testuser",
+    message: "testuser님이 입장하였습니다.",
+  },
+  {
+    createdDate: "2022-10-04T13:17:31",
+    id: 23,
+    messageType: 1,
+    roomId: 5,
+    sender: "test32",
+    message: "asdasdasd",
+  },
+];
--- front/src/atoms.tsx
+++ front/src/atoms.tsx
@@ -1,3 +1,25 @@
 import { atom } from "recoil";
+import { TypeMessage, TypeRoom, userData } from "./api";
 
 export const isDarkAtom = atom({ key: "isDark", default: false });
+
+export const isLoginAtom = atom({
+  key: "isLogin",
+  default:
+    localStorage.length !== 0 &&
+    localStorage["token"] !== "" &&
+    localStorage["token"] !== undefined,
+});
+
+export const userInfoAtom = atom<userData>({ key: "userInfo", default: null });
+
+export const roomsAtom = atom<TypeRoom[]>({ key: "rooms", default: [] });
+export const currentRoomIdAtom = atom<number | null | undefined>({
+  key: "roomId",
+  default: null,
+});
+export const messagesAtom = atom<TypeMessage[]>({
+  key: "messages",
+  default: [],
+});
+export const newChatModalAtom = atom({ key: "newChatModal", default: false });
