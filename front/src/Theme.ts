8ac433b57dd2b1a6c24495b96a375d0a7ffaf3fe
Jooyul Yoon <juyeolyoon@gmail.com>
2022-10-09 13:30:19 +0000

    221009 - dark theme partially added

--- front/src/Theme.ts
+++ front/src/Theme.ts
@@ -1,15 +1,29 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#4079ac",
-  textColor: "whitesmoke",
+  bgColor: "#202124",
+  textColor: "white",
+  subTextColor: "#9aa0a6",
+  redTextColor: "",
   mainColor: "black",
   subColor: "gray",
+  navbarColor: "#cf9e00",
+  inputColor: "#fff",
+  tableHeaderColor: "white",
+  tableBodyColor: "white",
+  hoverColor: "#52525245",
 };
 
 export const lightTheme: DefaultTheme = {
   bgColor: "#ffffff",
-  textColor: "#222222",
+  textColor: "rgba(0,0,0,.87)",
+  subTextColor: "rgba(55,53,47,0.65)",
+  redTextColor: "#ff6161",
   mainColor: "white",
   subColor: "gray",
+  navbarColor: "#fff",
+  inputColor: "rgba(242, 241, 238, 0.6)",
+  tableHeaderColor: "white",
+  tableBodyColor: "white",
+  hoverColor: "#e6eef5",
 };
--- front/src/styled.d.ts
+++ front/src/styled.d.ts
@@ -6,7 +6,14 @@ declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
     textColor: string;
+    subTextColor: string;
+    redTextColor: string;
     mainColor: string;
     subColor: string;
+    navbarColor: string;
+    inputColor: string;
+    tableHeaderColor: string;
+    tableBodyColor: string;
+    hoverColor: string;
   }
 }
