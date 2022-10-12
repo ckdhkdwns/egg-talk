3b05dad69fd6887b8e4d105107c899264bb612a1
Jooyul Yoon <juyeolyoon@gmail.com>
2022-10-12 13:40:22 +0000

    221012 - removed test data

--- front/src/api.ts
+++ front/src/api.ts
@@ -36,37 +36,3 @@ export type TypeMessage = {
   roomId: number;
   displayname: string;
 };
-
-export const roomsTest = [
-  {
-    createdDate: "2022-10-04T13:38:50",
-    roomId: 10,
-    creatorId: "test32",
-    roomName: "testterdf",
-  },
-  {
-    createdDate: "2022-10-04T13:53:51",
-    roomId: 11,
-    creatorId: "ckdhkdwns",
-    roomName: "My room!",
-  },
-];
-
-export const messagesTest = [
-  {
-    createdDate: "2022-10-04T10:31:19",
-    id: 4,
-    messageType: 0,
-    roomId: 6,
-    sender: "testuser",
-    message: "testuser님이 입장하였습니다.",
-  },
-  {
-    createdDate: "2022-10-04T13:17:31",
-    id: 23,
-    messageType: 1,
-    roomId: 5,
-    sender: "test32",
-    message: "asdasdasd",
-  },
-];
